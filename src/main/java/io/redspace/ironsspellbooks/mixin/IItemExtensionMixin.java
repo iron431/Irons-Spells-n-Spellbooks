304913e31849fc4959a3bfc54effa7a687a151c8
iron431 <34083081+iron431@users.noreply.github.com>
2024-11-12 00:11:55 -0600

    item->itemstack mixin

--- src/main/java/io/redspace/ironsspellbooks/mixin/IItemExtensionMixin.java
+++ src/main/java/io/redspace/ironsspellbooks/mixin/IItemExtensionMixin.java
@@ -3,25 +3,24 @@ package io.redspace.ironsspellbooks.mixin;
 
 import io.redspace.ironsspellbooks.registries.MobEffectRegistry;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.item.ItemStack;
-import net.neoforged.neoforge.common.extensions.IItemExtension;
+import net.neoforged.neoforge.common.extensions.IItemStackExtension;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(value = IItemExtension.class, remap = false, priority = 0)
+@Mixin(value = IItemStackExtension.class, remap = false, priority = 0)
 public interface IItemExtensionMixin {
 
     @Inject(method = "canElytraFly", at = @At(value = "RETURN"), cancellable = true, remap = false)
-    default void canElytraFly(ItemStack stack, LivingEntity entity, CallbackInfoReturnable<Boolean> cir) {
+    default void canElytraFly(LivingEntity entity, CallbackInfoReturnable<Boolean> cir) {
         if (entity.hasEffect(MobEffectRegistry.ANGEL_WINGS)) {
             cir.setReturnValue(true);
         }
     }
 
     @Inject(method = "elytraFlightTick", at = @At(value = "RETURN"), cancellable = true, remap = false)
-    default void elytraFlightTick(ItemStack stack, LivingEntity entity, int flightTicks, CallbackInfoReturnable<Boolean> cir) {
+    default void elytraFlightTick(LivingEntity entity, int flightTicks, CallbackInfoReturnable<Boolean> cir) {
         if (entity.hasEffect(MobEffectRegistry.ANGEL_WINGS)) {
             cir.setReturnValue(true);
         }
